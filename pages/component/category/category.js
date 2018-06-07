aea04139bafc3b9887b190e3af6f8e453d86170a
liangfapeng <Fredtwins@163.com>
2018-06-07 16:41:44 +0800

    分类

--- pages/component/category/category.js
+++ pages/component/category/category.js
@@ -4,7 +4,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    category: [
+      { name: '果味', id: 'guowei' },
+      { name: '蔬菜', id: 'shucai' },
+      { name: '炒货', id: 'chaohuo' },
+      { name: '点心', id: 'dianxin' },
+      { name: '粗茶', id: 'cucha' },
+      { name: '淡饭', id: 'danfan' }
+    ],
+    detail: [],
+    curIndex: 0,
+    isScroll: false,
+    toView: 'guowei'
   },
 
   /**
@@ -18,7 +29,35 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    var that = this;
+    wx.request({
+      url: 'http://www.gdfengshuo.com/api/wx/cate-detail.txt',
+      success(res) {
+          // console.log(res)
+        that.setData({
+          detail:res.data
+        })
+      }
+    })
+  },
+  
+  switchTab(e) {
+    console.log(e);
+    const that = this;
+    this.setData({
+      isScroll:true
+    })
+    setTimeout(function() {
+      that.setData({
+        toView: e.target.dataset.id,
+        curIndex: e.target.dataset.index
+      })
+    },0)
+    setTimeout(function() {
+      that.setData({
+        isScroll:false
+      })
+    },1)
   },
 
   /**
